624f78fc3ac48cc31acc6b1323131b7823e3c62f
Marek Vcelak <vcelak.marek@gmail.com>
2022-11-24 01:35:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,14 @@
-import React from "react";
-import ReactDOM from "react";
+import { createRoot } from "react-dom/client";
+import NewGameMenu from "./NewGameMenu";
+
+const App = () => {
+    return (
+        <div>
+            <NewGameMenu />
+        </div>
+    );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />)
