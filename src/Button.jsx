1a8d6a18b82f22cb01678907f20fe354c91b6091
Marek Včelák <vcelak.marek@gmail.com>
2022-11-26 23:28:42 +0100

    Update Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,15 +1,31 @@
 import "./styles.css";
 import "./button-styles.css"
 
-function Button(props){
-    const myClass = `button ${props.type}`
-    return(
-    <button 
-    className={myClass} 
-    onClick={props.handleClick}
-    >
-    {props.children}
-    </button>
+const color = {
+    primary: "yellow",
+    secondary: "silver"
+}
+
+const size = {
+    primary: "long" 
+}
+
+const border = {
+    primary: "border-none"
+}
+
+const Button ({ variant = "primary" }) => {
+
+    let baseClasses = [
+        color[variant],
+        size[variant],
+        border[variant]
+    ]
+
+    return (
+        <button
+        className={baseClasses.join(" ")}>
+        </button>
     )
 }
 
