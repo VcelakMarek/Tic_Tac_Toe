9febd14aaf122457fe39c21424c5c2dbf288722b
Marek Včelák <vcelak.marek@gmail.com>
2022-11-28 03:23:22 +0100

    Update ChooseSymbol.jsx

--- src/ChooseSymbol.jsx
+++ src/ChooseSymbol.jsx
@@ -1,38 +1,40 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import { OptionContext } from "./Option.context";
 import "./styles.css"
 import "./ChooseSymbol-styles.css"
+import Button from "./Button";
 
 const ChooseSymbol = () => {
     const [option, setOption] = useContext(OptionContext)
 
-    // X - true, Y - false
-    // "OPTIONX | OPTIONY"
-    // const [option, setOption] = useState("X");
-    // let optionO = "inactive";
-
     return (
-        <div className="card">
-            <h1>PICK PLAYER 1`S MARK</h1>
-            <div className="switch">
-                <div className="switch-option">
-                    <button 
-                        className={option === "X" ? "active" : "inactive"}
-                        onClick={() => setOption("X")}
-                        >
-                        <img  src="./assets/icon-x-silver.svg" alt="icon-x" />
-                    </button> 
+        <div>
+            <div className="card">
+                <h1>PICK PLAYER 1`S MARK</h1>
+                <div className="switch">
+                    <div className="switch-option">
+                        <button 
+                            className={option === "X" ? "active" : "inactive"}
+                            onClick={() => setOption("X")}
+                            >
+                            <img  src="./assets/icon-x-silver.svg" alt="icon-x" />
+                        </button> 
+                    </div>
+                    <div className="switch-option">
+                        <button
+                            className={option === "Y" ? "active" : "inactive"}
+                            onClick={() => setOption("Y")}
+                            >
+                            <img src="./assets/icon-o-silver.svg" alt="icon-o" />
+                        </button>
+                    </div>
                 </div>
-                <div className="switch-option">
-                    <button
-                        className={option === "Y" ? "active" : "inactive"}
-                        onClick={() => setOption("Y")}
-                        >
-                        <img src="./assets/icon-o-silver.svg" alt="icon-o" />
-                    </button>
-                </div>
+                <h2>REMEMBER: X GOES FIRST</h2>
             </div>
-            <h2>REMEMBER: X GOES FIRST</h2>
+            <div className="new-game-buttons">
+                <Button variant="newGameCPU"></Button>
+                <Button variant="newGamePlayer" />
+            </div>
         </div>
     )
 }
