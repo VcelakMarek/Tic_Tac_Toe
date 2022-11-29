2a9db49bf51ddccbf43425880534ea749d486570
Marek Vcelak <vcelak.marek@gmail.com>
2022-11-29 01:24:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import OptionProvider from "./Option.context";
 import NewGameMenu from "./NewGameMenu";
 import Game from "./Game";
 
 const App = () => {
     return (
-        <OptionProvider>
-            <div>
-               <NewGameMenu /> 
-              {/* <Game />*/}
-            </div>
-
-        </OptionProvider>
-
+        <BrowserRouter>
+            <OptionProvider>
+                <Routes>
+                    <Route path="/" element={<NewGameMenu />} />
+                    <Route path="/game" element={<Game />} />
+                </Routes>
+            </OptionProvider>
+        </BrowserRouter>
     );
 };
 
