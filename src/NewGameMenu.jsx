33142e7f971634cfbe8675fd19ee607fc1d531c2
Marek Vcelak <vcelak.marek@gmail.com>
2022-11-29 01:26:30 +0100

    Update NewGameMenu.jsx

--- src/NewGameMenu.jsx
+++ src/NewGameMenu.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./NewGameMenu-styles.css"
 import ChooseSymbol from "./ChooseSymbol";
+import Button from "./Button";
 
 const NewGameMenu = () => {
+
+    const [vsCPU, setVsCPU] = useState();
+    console.log(vsCPU);
     return (
         <div>
-            <img src="./assets/icon-x.svg" alt="icon-x" />
-            <img src="./assets/icon-o.svg" alt="icon-o" />
+            <div className="XO-header">
+                <img src="./assets/icon-x.svg" alt="icon-x" />
+                <img src="./assets/icon-o.svg" alt="icon-o" />
+            </div>
             <ChooseSymbol />
+            <div className="new-game-buttons">
+                <Link to="/game">
+                    <Button 
+                        variant="newGameCPU"
+                        onclick={() => setVsCPU(true)}
+                    />
+                </Link>
+                <Link to="/game">
+                    <Button 
+                        variant="newGamePlayer" 
+                        onclick={() => setVsCPU(false)}    
+                    />
+                </Link>
+            </div>
         </div>
     )
 }
