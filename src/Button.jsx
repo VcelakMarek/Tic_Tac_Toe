f770c1b067c678dac71b0308fc360afb9e6f610b
Marek Vcelak <vcelak.marek@gmail.com>
2022-12-01 22:57:11 +0100

    Update Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { OptionContext } from "./Option.context";
+import { MyContext } from "./MyContext";
 
 import "./styles.css";
 import "./button-styles.css"
@@ -34,15 +35,14 @@ const text = {
     newGamePlayer: "NEW GAME  (VS PLAYER)"
 }
 
-const Button = ( {variant = "gameButton", ...props} ) => {
 
-    const [option, setOption] = useContext(OptionContext);
+const Button = ( {variant = "gameButton", children, ...props} ) => {
+    
+    const [option] = useContext(OptionContext);
     const [hoverX, setHoverX] = useState("invisible");
     const [hoverO, setHoverO] = useState("invisible");
-    const [displayMark, setDisplayMark] = useState("invisible");
-
-
-    
+    const [displayX] = useContext(MyContext);
+console.log("displayX",displayX)
     let baseClasses = [
         color[variant],
         size[variant],
@@ -65,23 +65,28 @@ const Button = ( {variant = "gameButton", ...props} ) => {
             onMouseLeave={() => option === "X" ? setHoverX("invisible") : setHoverO("invisible")}
             onClick={props.onclick}
             className={baseClasses.join(" ")}>
-            <img className={option === "X" ? displayMark : "invisible"} src="./assets/icon-x.svg" alt="icon-x"></img>
-            <img className={option === "O" ? displayMark : "invisible"} src="./assets/icon-o.svg" alt="icon-o"></img>
-            <img className={hoverX} src="./assets/icon-x-outline.svg" alt="icon-x-outline"></img>
-            <img className={hoverO} src="./assets/icon-o-outline.svg" alt="icon-o-outline"></img>
+                <img className="invisible" src="./assets/icon-x.svg" alt="icon-x"></img>
+                <img className="invisible" src="./assets/icon-o.svg" alt="icon-o"></img>
+                <img className={hoverX} src="./assets/icon-x-outline.svg" alt="icon-x-outline"></img>
+                <img className={hoverO} src="./assets/icon-o-outline.svg" alt="icon-o-outline"></img>
             </button>
             )
     
-    } else {
+    } else if (variant === "newGameCPU" || variant === "newGamePlayer") {
         return (
-
             <button
             className={baseClasses.join(" ")}>
-            <p>{text[variant]}</p>
+                 <p>{text[variant]}</p>
             </button>
         )
-
-}
+    } else {
+        return (
+            <button
+            className={baseClasses.join(" ")}>
+                {children}
+            </button>
+        )
+    }
 }
 
 export default Button;
