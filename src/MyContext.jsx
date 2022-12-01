23e6c91a5daad81c16daad21fa851dc916c54fc7
Marek Vcelak <vcelak.marek@gmail.com>
2022-12-01 22:57:26 +0100

    Create MyContext.jsx

---
+++ src/MyContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const MyContext = createContext("");
+
+const MyProvider = props => {
+  const [displayX, setDisplayX] = useState("invisible");
+  const [displayO, setDisplayO] = useState("invisible");
+
+  return (
+    <MyContext.Provider
+      value={{ displayX: [displayX, setDisplayX], displayO: [displayO, setDisplayO] }}
+    >
+      {props.children}
+    </MyContext.Provider>
+  );
+};
+
+export default MyProvider;
