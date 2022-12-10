c25a1172dfabf9dccf03c3e80ab8db91f1111d99
Marek Včelák <vcelak.marek@gmail.com>
2022-12-10 20:08:25 +0100

    Update Modal.jsx

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,65 +1,67 @@
-import { useContext } from "react"
-import CreateModal from "./CreateModal"
-import Button from "./Button"
-import reset from "./reset"
-import nextRound from "./nextRound"
-import { MyContext } from "./MyContext"
-import "./Modal-styles.css"
+import { useContext } from "react";
+import CreateModal from "./CreateModal";
+import Button from "./Button";
+import reset from "./reset";
+import nextRound from "./nextRound";
+import { MyContext } from "./MyContext";
+import "./Modal-styles.css";
 
 const text = {
-    xWin: "TAKES THE ROUND",
-    oWin: "TAKES THE ROUND",
-    tied: "ROUND TIED",
-    restartGame: "RESTART GAME"
-}
+  xWin: "TAKES THE ROUND",
+  oWin: "TAKES THE ROUND",
+  tied: "ROUND TIED",
+  restartGame: "RESTART GAME",
+};
 
 const color = {
-    xWin: "color-light-blue",
-    oWin: "color-light-yellow"
-}
+  xWin: "color-light-blue",
+  oWin: "color-light-yellow",
+};
 
 const img = {
-    xWin: <img src="./assets/icon-x.svg" alt="icon-x"></img>,
-    oWin: <img src="./assets/icon-o.svg" alt="icon-o"></img>
-}
+  xWin: <img src="./assets/icon-x.svg" alt="icon-x"></img>,
+  oWin: <img src="./assets/icon-o.svg" alt="icon-o"></img>,
+};
 
 // const link = {
 
 // }
 
 const winInfo = {
-    lost: "OH NO, YOU LOST…",
-    won: "YOU WON!",
-    player1: "PLAYER 1 WINS!",
-    player2: "PLAYER 2 WINS!"
-}
+  lost: "OH NO, YOU LOST…",
+  won: "YOU WON!",
+  player1: "PLAYER 1 WINS!",
+  player2: "PLAYER 2 WINS!",
+};
 
-const Modal = ({variant, ...props}) => {
-    return (
-        <CreateModal>
-            <div className={color[variant]}>
-                <h2>{winInfo[props.winner]}</h2>
-                <h1>
-                    <p>{img[variant]}</p>
-                    <p>{text[variant]}</p>
-                </h1>
-                <div className="buttons">
-                    <Button 
-                        link={variant === "restartGame" ? "/game" : "/"}
-                        variant="silver" 
-                        onclick={props.onclick}>
-                        <p>{variant === "restartGame" ? ("NO, CANCEL") : ("QUIT")}</p>
-                    </Button>
-                    <Button 
-                        link={variant != "restartGame" ? "/game" : "/"}
-                        variant="yellow"
-                        onclick={props.onclick}>
-                        <p>{variant === "restartGame" ? ("YES, RESTART") : ("NEXT ROUND")}</p>
-                    </Button>
-                </div>
-            </div>
-        </CreateModal>
-    )
-}
+const Modal = ({ variant, ...props }) => {
+  return (
+    <CreateModal>
+      <div className={color[variant]}>
+        <h2>{winInfo[props.winner]}</h2>
+        <h1>
+          <p>{img[variant]}</p>
+          <p>{text[variant]}</p>
+        </h1>
+        <div className="buttons">
+          <Button
+            link={variant === "restartGame" ? "/game" : "/"}
+            variant="silver"
+            onclick={props.onclick}
+          >
+            <p>{variant === "restartGame" ? "NO, CANCEL" : "QUIT"}</p>
+          </Button>
+          <Button
+            link={variant != "restartGame" ? "/game" : "/"}
+            variant="yellow"
+            onclick={props.onclick}
+          >
+            <p>{variant === "restartGame" ? "YES, RESTART" : "NEXT ROUND"}</p>
+          </Button>
+        </div>
+      </div>
+    </CreateModal>
+  );
+};
 
 export default Modal;
