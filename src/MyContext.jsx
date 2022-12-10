80d453e14708b8f15de20bd955a82a7ace55feff
Marek Včelák <vcelak.marek@gmail.com>
2022-12-10 20:08:22 +0100

    Update MyContext.jsx

--- src/MyContext.jsx
+++ src/MyContext.jsx
@@ -2,13 +2,16 @@ import { createContext, useState } from "react";
 
 export const MyContext = createContext("");
 
-const MyProvider = props => {
+const MyProvider = (props) => {
   const [displayX, setDisplayX] = useState("invisible");
   const [displayO, setDisplayO] = useState("invisible");
 
   return (
     <MyContext.Provider
-      value={{ displayX: [displayX, setDisplayX], displayO: [displayO, setDisplayO] }}
+      value={{
+        displayX: [displayX, setDisplayX],
+        displayO: [displayO, setDisplayO],
+      }}
     >
       {props.children}
     </MyContext.Provider>
