1fac9292db62fc2bb151d96bf60abb9a6c657add
Marek Včelák <vcelak.marek@gmail.com>
2022-12-10 20:09:35 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import OptionProvider from "./Option.context";
+import VsCpuProvider from "./vsCpu.context";
 import NewGameMenu from "./NewGameMenu";
 import Game from "./Game";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <OptionProvider>
-                <Routes>
-                    <Route path="/" element={<NewGameMenu />} />
-                    <Route path="/game" element={<Game />} />
-                </Routes>
-            </OptionProvider>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <OptionProvider>
+        <VsCpuProvider>
+          <Routes>
+            <Route path="/" element={<NewGameMenu />} />
+            <Route path="/game" element={<Game />} />
+          </Routes>
+        </VsCpuProvider>
+      </OptionProvider>
+    </BrowserRouter>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />)
+root.render(<App />);
