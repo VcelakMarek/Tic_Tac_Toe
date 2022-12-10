08e4017b358ba6f488cd207725f8fcda0874f921
Marek Včelák <vcelak.marek@gmail.com>
2022-12-10 20:08:19 +0100

    Update NewGameMenu.jsx

--- src/NewGameMenu.jsx
+++ src/NewGameMenu.jsx
@@ -1,36 +1,29 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-import "./NewGameMenu-styles.css"
+import "./NewGameMenu-styles.css";
 import ChooseSymbol from "./ChooseSymbol";
 import Button from "./Button";
+import { vsCpuContext } from "./vsCpu.context";
 
 const NewGameMenu = () => {
-
-    const [vsCPU, setVsCPU] = useState();
-    console.log(vsCPU);
-    return (
-        <div>
-            <div className="XO-header">
-                <img src="./assets/icon-x.svg" alt="icon-x" />
-                <img src="./assets/icon-o.svg" alt="icon-o" />
-            </div>
-            <ChooseSymbol />
-            <div className="new-game-buttons">
-                <Link to="/game">
-                    <Button 
-                        variant="newGameCPU"
-                        onclick={() => setVsCPU(true)}
-                    />
-                </Link>
-                <Link to="/game">
-                    <Button 
-                        variant="newGamePlayer" 
-                        onclick={() => setVsCPU(false)}    
-                    />
-                </Link>
-            </div>
-        </div>
-    )
-}
+  const [vsCPU, setVsCPU] = useContext(vsCpuContext);
+  return (
+    <div>
+      <div className="XO-header">
+        <img src="./assets/icon-x.svg" alt="icon-x" />
+        <img src="./assets/icon-o.svg" alt="icon-o" />
+      </div>
+      <ChooseSymbol />
+      <div className="new-game-buttons">
+        <Link to="/game">
+          <Button variant="newGameCPU" onClick={() => setVsCPU(true)} />
+        </Link>
+        <Link to="/game">
+          <Button variant="newGamePlayer" onClick={() => setVsCPU(false)} />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default NewGameMenu;
