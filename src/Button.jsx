7392731da4cc91bffce968a9abb02e597bb511c7
Marek Včelák <vcelak.marek@gmail.com>
2022-12-10 20:08:39 +0100

    Update Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -4,98 +4,108 @@ import { OptionContext } from "./Option.context";
 import { MyContext } from "./MyContext";
 
 import "./styles.css";
-import "./button-styles.css"
+import "./button-styles.css";
 
 const color = {
-    newGameCPU: "yellow",
-    newGamePlayer: "blue",
-    silver: "silver",
-    yellow: "yellow box-shadow-4px",
-    restart: "silver",
-    gameButton: "dark-navy"
-}
+  newGameCPU: "yellow",
+  newGamePlayer: "blue",
+  silver: "silver",
+  yellow: "yellow box-shadow-4px",
+  restart: "silver",
+  gameButton: "dark-navy",
+};
 
 const size = {
-    newGameCPU: "long",
-    newGamePlayer: "long",
-    restart: "small",
-    gameButton: "game-button-size",
-    silver: "height-52px",
-    yellow: "height-52px"
-}
+  newGameCPU: "long",
+  newGamePlayer: "long",
+  restart: "small",
+  gameButton: "game-button-size",
+  silver: "height-52px",
+  yellow: "height-52px",
+};
 
 const border = {
-    newGameCPU: "border-radius-15px",
-    newGamePlayer: "border-radius-15px",
-    silver: "border-radius-10px",
-    yellow: "border-radius-10px",
-    restart: "border-radius-5px",
-    gameButton: "border-radius-15px"
-}
+  newGameCPU: "border-radius-15px",
+  newGamePlayer: "border-radius-15px",
+  silver: "border-radius-10px",
+  yellow: "border-radius-10px",
+  restart: "border-radius-5px",
+  gameButton: "border-radius-15px",
+};
 
 const text = {
-    newGameCPU: "NEW GAME  (VS CPU)",
-    newGamePlayer: "NEW GAME  (VS PLAYER)"
-}
+  newGameCPU: "NEW GAME  (VS CPU)",
+  newGamePlayer: "NEW GAME  (VS PLAYER)",
+};
 
+const Button = ({ variant = "gameButton", value, children, ...props }) => {
+  const [option] = useContext(OptionContext);
+  const [hoverX, setHoverX] = useState("invisible");
+  const [hoverO, setHoverO] = useState("invisible");
+  // const [displayX] = useContext(MyContext);
+  console.log(value);
+  let baseClasses = [color[variant], size[variant], border[variant]];
 
-const Button = ( {variant = "gameButton", children, ...props} ) => {
-    
-    const [option] = useContext(OptionContext);
-    const [hoverX, setHoverX] = useState("invisible");
-    const [hoverO, setHoverO] = useState("invisible");
-    // const [displayX] = useContext(MyContext);
+  if (variant === "restart") {
+    return (
+      <button className={baseClasses.join(" ")} onClick={props.onClick}>
+        <img src="./assets/icon-restart.svg" alt="restart-icon" />
+      </button>
+    );
+  } else if (variant === "gameButton") {
+    return (
+      <button
+        onMouseEnter={() =>
+          option === "X" ? setHoverX("visible") : setHoverO("visible")
+        }
+        onMouseLeave={() =>
+          option === "X" ? setHoverX("invisible") : setHoverO("invisible")
+        }
+        onClick={(e) => {
+          props.onclick(e);
+          setHoverX("invisible");
+          setHoverO("invisible");
+        }}
+        className={baseClasses.join(" ")}
+      >
+        {value === "X" && (
+          <img className="visible" src="./assets/icon-x.svg" alt="icon-x" />
+        )}
+        {value === "O" && (
+          <img className="visible" src="./assets/icon-o.svg" alt="icon-o" />
+        )}
 
-    let baseClasses = [
-        color[variant],
-        size[variant],
-        border[variant]
-    ]
-
-    if (variant === "restart"){
-        return(
-            <button
-            className={baseClasses.join(" ")}
-            onClick={props.onClick}>
-            <img  src="./assets/icon-restart.svg" alt="restart-icon" />
-            </button>
-            )
-
-    }  else if (variant === "gameButton") { 
-        return(
-            <button 
-            // value={props.value}
-            disabled = {false}
-            onMouseEnter={() => option === "X" ? setHoverX("visible") : setHoverO("visible")}
-            onMouseLeave={() => option === "X" ? setHoverX("invisible") : setHoverO("invisible")}
-            onClick={(e) => {props.onclick(e);props.disabled="true" }}
-            // {...props.allowHover}
-            className={baseClasses.join(" ")}>
-                <img className="invisible" src="./assets/icon-x.svg" alt="icon-x"></img>
-                <img className="invisible" src="./assets/icon-o.svg" alt="icon-o"></img>
-                <img className={hoverX} src="./assets/icon-x-outline.svg" alt="icon-x-hover"></img>
-                <img className={hoverO} src="./assets/icon-o-outline.svg" alt="icon-o-hover"></img>
-            </button>
-            )
-    
-    } else if (variant === "newGameCPU" || variant === "newGamePlayer") {
-        return (
-            <button
-            className={baseClasses.join(" ")}>
-                 <p>{text[variant]}</p>
-            </button>
-        )
-    } else {
-        return (
-            <Link to={props.link}>
-                <button
-                className={baseClasses.join(" ")}
-                onClick={props.onclick}>
-                    {children}
-                </button>
-            </Link>
-        )
-    }
-}
+        {value === "" && hoverX === "visible" && (
+          <img
+            className="visible"
+            src="./assets/icon-x-outline.svg"
+            alt="icon-x-hover"
+          />
+        )}
+        {value === "" && hoverO === "visible" && (
+          <img
+            className="visible"
+            src="./assets/icon-o-outline.svg"
+            alt="icon-o-hover"
+          />
+        )}
+      </button>
+    );
+  } else if (variant === "newGameCPU" || variant === "newGamePlayer") {
+    return (
+      <button className={baseClasses.join(" ")} onClick={props.onClick}>
+        <p>{text[variant]}</p>
+      </button>
+    );
+  } else {
+    return (
+      <Link to={props.link}>
+        <button className={baseClasses.join(" ")} onClick={props.onclick}>
+          {children}
+        </button>
+      </Link>
+    );
+  }
+};
 
 export default Button;
