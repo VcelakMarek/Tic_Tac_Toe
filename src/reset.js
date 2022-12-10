931b8868bed8968484c548e7bee89df40d9541af
Marek Včelák <vcelak.marek@gmail.com>
2022-12-10 20:08:09 +0100

    Update reset.js

--- src/reset.js
+++ src/reset.js
@@ -1,19 +1,21 @@
-function reset(setBoardValues, setP1Wins, setP2Wins,setWinner) {
-    const images = Array.from(
-        document.querySelectorAll('.game-board button img')
-      );
-  
-      images.forEach(element => {
-        element.className = 'invisible';
+function reset(setBoardValues, setP1Wins, setP2Wins, setWinner) {
+  const images = Array.from(
+    document.querySelectorAll(".game-board button img")
+  );
 
-    setBoardValues(([
-        ["" ,"" ,""],
-        ["", "", ""],
-        ["", "", ""]
-    ]))
-    setP1Wins(0);
-    setP2Wins(0);
-    setWinner("");
-})}
+  setBoardValues([
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+  setP1Wins(0);
+  setP2Wins(0);
+  setWinner("");
+
+  // images.forEach((element) => {
+  //   element.className = "invisible";
+  //   console.log("element", element);
+  // });
+}
 
 export default reset;
